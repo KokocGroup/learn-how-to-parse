b0150ed8b82fcc9729a1b00ed371f3a488ee5113
Alexey Ruzin <ruzin@kokoc.com>
2022-05-26 11:21:49 +0300

    regexes

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
 import re
 
-url = 'http://kokoc.com/'
 
-reqeust = requests.get(url)
+main_url = 'https://kokoc.com'
+
+request = requests.get(main_url)
 
 # with open('kokoc.html', 'w') as f:
-#     f.write(reqeust.text)
+#     f.write(request.text)
 
-if 'Google Tag Manager' in reqeust.text:
-    print('Google Tag Manager is present')
+# if 'Google Tag Manager' in request.text:
+#     print('Google Tag Manager is present')
 
+
+match = re.search(r"Google Tag Manager -->(.*?'(GTM-[0-9a-zA-Z]+)'.*?)<!-- End Google Tag Manager", request.text, re.DOTALL)
+if match is not None:
+    print(match.group(1))
+    gtm = match.group(2)
+else:
+    print('ничего не найдено')
 
 print(reqeust.text.count('Google Tag Manager'))
