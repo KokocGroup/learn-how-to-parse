ec7131bd66b0f429a1e4b2570b593e81c2e8253c
Alexey Ruzin <ruzin@kokoc.com>
2022-06-09 10:40:03 +0300

    test

--- check_gtm.py
+++ check_gtm.py
@@ -23,21 +23,22 @@ if __name__ == '__main__':
     gtm_differ = []
 
     cnt = 0
-    while(urls_to_download):
-        url = list(urls_to_download)[0]
-        urls_to_download.remove(url)
-        downloaded_urls.add(url)
+    while cnt> 0:
+        while(urls_to_download):
+            url = list(urls_to_download)[0]
+            urls_to_download.remove(url)
+            downloaded_urls.add(url)
 
-        page_content = get_page_content(main_url+url)
-        gtm = extract_gtm(page_content)
-        if gtm == main_gtm:
-            gtm_same.append(url)
-        else:
-            gtm_differ.append(url)
-        cnt += 1
-        new_links = get_links(page_content, main_url+url)
+            page_content = get_page_content(main_url+url)
+            gtm = extract_gtm(page_content)
+            if gtm == main_gtm:
+                gtm_same.append(url)
+            else:
+                gtm_differ.append(url)
+            cnt += 1
+            new_links = get_links(page_content, main_url+url)
 
-        urls_to_download |= new_links - downloaded_urls
+            urls_to_download |= new_links - downloaded_urls
 
     print(f'На {len(gtm_differ)} страницах GTM отличается от главной страницы')
     for url in gtm_differ:
